214eed9a40b8fa632aa16062a5e21d13ca9e3fc0
tom creighton <tjcreighton@gmail.com>
2012-11-07 10:26:25 -0500

    initial updates to golf example for 0.95

---
+++ 0.95/original_prototypes/GolfExample_TCAPI/scripts/common.js
@@ -0,0 +1,23 @@
+GolfExample = { };
+
+GolfExample.CourseActivity = 
+{
+    "id":"adlnet.gov/GolfExample_TCAPI", 
+    "definition":{
+    	"type":"Course",
+    	"name":{"en-US":"Golf Example - Tin Can Course"},
+    	"description":{"en-US":"An overview of how to play the great game of golf."}
+    }
+};
+
+GolfExample.getContext = function(parentActivityId){
+    var ctx = {
+        "contextActivities":{
+            "grouping":{"id":GolfExample.CourseActivity.id}
+        }
+    };
+    if(parentActivityId !== undefined && parentActivityId !== null){
+        ctx.contextActivities["parent"] = {"id":parentActivityId};
+    }
+    return ctx;
+};
